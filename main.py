44c6499ccd53e58f24ba32d6fcdefa31d70bc25b
ivanbard <ivanbardziyan@gmail.com>
2025-10-14 14:48:57 -0400

    just pulling basic market data

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 CG_KEY = os.getenv("COINGECKO-KEY")
 
-url = "https://api.coingecko.com/api/v3/coins/markets"
-headers = {"x-cg-demo-api-key": "{CG_KEY}"}
+url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
+headers = {"x-cg-demo-api-key": CG_KEY}
 
-print(f"{CG_KEY}")
+# print(f"{CG_KEY}")
+
+response = requests.get(url, headers=headers)
+print(response.json())

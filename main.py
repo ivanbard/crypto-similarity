5dca0fe44da3f8a8dc8543f86e08a3a4ac04fde1
ivanbard <ivanbardziyan@gmail.com>
2025-12-09 22:30:13 -0500

    recreating program into functions with messari for crypto features

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-import os, requests, json
+import os, requests, json, time
 import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv()
+
 CG_KEY = os.getenv("COINGECKO-KEY")
+MESSARI_KEY = os.getenv("MESSARI-KEY")
 
 url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&page={}"
-headers = {"x-cg-demo-api-key": CG_KEY}
-
-# print(f"{CG_KEY}")
-
-all_data = []
-for page in range(1,3):
-    url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&page={page}"
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    all_data.extend(data)
-    print(f"fetched page {page}: {len(data)} coins")
 
 
-df = pd.DataFrame(all_data)
-print(df)
-df.to_csv('crypto.csv', index=False)
+def fetch_coingecko(pages=3):
+    headers = {"x-cg-demo-api-key": CG_KEY}
+    all_data = []
+
+    for page in range(1, pages+1):
+        url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&page={page}"
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            all_data.extend(data)
+            print(f"fetched page {page}: {len(data)} coins")
+        else:
+            print("error fetching coingecko")
+
+        # pause ever iteration to prevent hitting rate limit
+        time.sleep(1) 
+    return pd.DataFrame(all_data)
+
+
+
+#for page in range(1,3):
+
+#df = pd.DataFrame(all_data)
+#print(df)
 #save to csv, so i have a proper dataset to work with rather than json bs
